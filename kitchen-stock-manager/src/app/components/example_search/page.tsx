2769e0624e76a48f1866936dcfac1485c0a983a0
Sunny <lovesunday1006@gmail.com>
2025-06-12 18:02:52 +0700

    feat : Test

--- kitchen-stock-manager/src/app/components/example_search/page.tsx
+++ kitchen-stock-manager/src/app/components/example_search/page.tsx
@@ -5,6 +5,7 @@ export default function Order() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
         <div className="w-full max-w-2xl">
+          Test
           <SearchBox />
         </div>
     </main>
