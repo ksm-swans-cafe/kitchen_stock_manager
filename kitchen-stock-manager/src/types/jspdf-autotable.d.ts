3ba369d97bd8f560de3eda4df16fd85c5ca52ab7
Sunny <lovesunday1006@gmail.com>
2025-06-16 06:58:17 +0700

    fix : any

--- kitchen-stock-manager/src/types/jspdf-autotable.d.ts
+++ kitchen-stock-manager/src/types/jspdf-autotable.d.ts
@@ -1,7 +1,9 @@
 import 'jspdf';
+import type { UserOptions } from 'jspdf-autotable';
+
 
 declare module 'jspdf' {
   interface jsPDF {
-    autoTable: (options: any) => jsPDF;
+    autoTable: (options: UserOptions) => jsPDF;
   }
 }
