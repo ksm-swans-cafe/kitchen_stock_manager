e0b1340f033da031c927474c4e06798f305b9a15
Chinanan Seelakum <lovesunday1006@gmail.com>
2025-12-24 20:57:37 +0700

    deploy vercel

--- kitchen-stock-manager/src/app/home/order/menu-picker/page.tsx
+++ kitchen-stock-manager/src/app/home/order/menu-picker/page.tsx
@@ -598,7 +598,7 @@ export default function Order() {
     };
   }, []);
 
-  // Show loading overlay when loading edit data
+  // Show loading overlay when loading edit data a
   if (isLoadingEditData) {
     return (
       <div className='min-h-screen bg-gradient-to-br from-gray-50 via-white to-gray-100 flex items-center justify-center p-4'>
